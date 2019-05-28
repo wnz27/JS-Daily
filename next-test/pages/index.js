90889f640201dadbe4ae9be6141fad3e3088aa56
Feng <wnz27@icloud.com>
2019-05-29 07:55:18 +0800

    update

--- next-test/pages/index.js
+++ next-test/pages/index.js
@@ -1,2 +1,26 @@
-export default () => <div>Welcome to next.js!</div>
+/* export default () => <div>Welcome to next.js!</div> */
+export default () =>
+  <div>
+    Hello world
+    <p>scoped!</p>
+    <style jsx>{`
+      p {
+        color: blue;
+      }
+      div {
+        background: red;
+      }
+      @media (max-width: 600px) {
+        div {
+          background: blue;
+        }
+      }
+    `}</style>
+    <style global jsx>{`
+      body {
+        background: black;
+      }
+    `}</style>
+    <img src="/static/my-image.png" alt="my image" />
+  </div>
 
