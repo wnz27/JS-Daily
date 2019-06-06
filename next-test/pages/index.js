c71cad4945125a6cde79911d5741bd33a4d7996f
Feng <wnz27@icloud.com>
2019-06-06 12:01:20 +0800

    next 基础结构，官网主页基础文件分块

--- next-test/pages/index.js
+++ next-test/pages/index.js
@@ -1,32 +1,19 @@
 /* export default () => <div>Welcome to next.js!</div> */
-import Head from 'next/head'
-export default () =>
+import Head from "next/head";
+import React from "react";
+import Link from "next/link";
+
+export default () => (
   <div>
     <Head>
-    <title>My page title</title>
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <title>My page title</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <p>Hello world!</p>
-    Hello world
-    <p>scoped!</p>
-    <style jsx>{`
-      p {
-        color: blue;
-      }
-      div {
-        background: red;
-      }
-      @media (max-width: 600px) {
-        div {
-          background: blue;
-        }
-      }
-    `}</style>
-    <style global jsx>{`
-      body {
-        background: black;
-      }
-    `}</style>
-    <img src="/static/my-image.png" alt="my image" />
+    Click{" "}
+    <Link href="/about">
+      <a>here</a>
+    </Link>{" "}
+    to read more
   </div>
-
+);
