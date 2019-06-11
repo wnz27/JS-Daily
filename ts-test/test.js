9c50b1a2b0bdd700b5e453b641c0208df11ec937
Feng <wnz27@icloud.com>
2019-06-11 18:23:08 +0800

    update

--- ts-test/test.js
+++ ts-test/test.js
@@ -1,4 +1,6 @@
 "use strict";
 var message = "Hello World";
 console.log(message);
+let str = "12345";
+console.log(str);
 //# sourceMappingURL=test.js.map
--- ts-test/test.ts
+++ ts-test/test.ts
@@ -1,2 +1,4 @@
 var message: string = "Hello World";
 console.log(message);
+let str: string = "12345";
+console.log(str);
