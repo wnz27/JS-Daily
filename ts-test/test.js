e03addc57e6a3e3373502a8ad6e435f35b2187be
Feng <wnz27@icloud.com>
2019-06-12 10:26:46 +0800

    update

--- ts-test/test.js
+++ ts-test/test.js
@@ -3,4 +3,9 @@ var message = "Hello World";
 console.log(message);
 let str = "12345";
 console.log(str);
+function greeter(person) {
+    return "Hello " + person.firstName + " " + person.lastName;
+}
+let user = { firstName: "Jane", lastName: "User" };
+console.log(greeter(user));
 //# sourceMappingURL=test.js.map
--- ts-test/test.ts
+++ ts-test/test.ts
@@ -2,3 +2,15 @@ var message: string = "Hello World";
 console.log(message);
 let str: string = "12345";
 console.log(str);
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Jane", lastName: "User" };
+console.log(greeter(user));
