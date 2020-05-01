2134a13a6651e7d71d070c3112e0794d5c4898e1
fzk27 <feng@littlelights.ai>
2020-05-01 14:58:29 +0800

    vscode设置代码段模板

---
+++ content/vue-test/vuejs从入门到项目实战/codeprac/test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>加油⛽️💪！！！！！</div>
+</template>
+
+<script>
+//import x from ''
+export default {
+  name: "27",
+  props: {},
+  data() {
+    return {};
+  },
+  components: {},
+  computed: {},
+  watch: {},
+  methods: {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  created() {}, // 生命周期 - 已经创建
+  beforeMount() {}, //生命周期 - 挂载之前
+  mounted() {}, //生命周期 - 已经挂载
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+
+<style scoped lang="scss">
+</style>
